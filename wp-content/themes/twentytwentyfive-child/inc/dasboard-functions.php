b1f8f11756a20c379ad49dfe58c751c3bba29a71
zion565 <zion321654@gmail.com>
2025-02-04 23:50:02 +0200

    תיקון השאילת להצגת נתונים מדוייקים

--- wp-content/themes/twentytwentyfive-child/inc/dasboard-functions.php
+++ wp-content/themes/twentytwentyfive-child/inc/dasboard-functions.php
@@ -17,18 +17,21 @@ function render_license_management_page() {
     global $wpdb;
 
     $licenses = $wpdb->get_results("
-        SELECT p.ID, p.post_date, pm1.meta_value AS license_key, pm2.meta_value AS license_status,
-               pm3.meta_value AS expiry_date, pm4.meta_value AS total_paid, o.total as order_total,
+        SELECT p.ID, p.post_status, p.post_date, 
+               pm1.meta_value AS license_key, 
+               pm2.meta_value AS license_status,
+               pm3.meta_value AS expiry_date, 
+               pm4.meta_value AS total_paid, 
+               o.meta_value as order_total, 
                c.display_name, c.user_email
-        FROM wp_posts p
-        JOIN wp_postmeta pm1 ON p.ID = pm1.post_id AND pm1.meta_key = '_license_key'
-        JOIN wp_postmeta pm2 ON p.ID = pm2.post_id AND pm2.meta_key = '_license_status'
-        JOIN wp_postmeta pm3 ON p.ID = pm3.post_id AND pm3.meta_key = '_license_expiry_date'
-        JOIN wp_postmeta pm4 ON p.ID = pm4.post_id AND pm4.meta_key = '_total_paid'
-        JOIN wp_users c ON p.post_author = c.ID
-        JOIN (SELECT post_id, meta_value as total FROM wp_postmeta WHERE meta_key = '_order_total') o 
-        ON p.ID = o.post_id
-        WHERE p.post_type = 'shop_order'
+        FROM {$wpdb->prefix}posts p
+        LEFT JOIN {$wpdb->prefix}postmeta pm1 ON p.ID = pm1.post_id AND pm1.meta_key = '_license_key'
+        LEFT JOIN {$wpdb->prefix}postmeta pm2 ON p.ID = pm2.post_id AND pm2.meta_key = '_license_status'
+        LEFT JOIN {$wpdb->prefix}postmeta pm3 ON p.ID = pm3.post_id AND pm3.meta_key = '_license_expiration'
+        LEFT JOIN {$wpdb->prefix}postmeta pm4 ON p.ID = pm4.post_id AND pm4.meta_key = '_total_paid'
+        LEFT JOIN {$wpdb->prefix}postmeta o ON p.ID = o.post_id AND o.meta_key = '_order_total'
+        LEFT JOIN {$wpdb->prefix}users c ON p.post_author = c.ID
+        WHERE p.post_type in ('shop_order','shop_order_placehold')
     ");
 
     echo "<div class='wrap'>";
@@ -38,8 +41,9 @@ function render_license_management_page() {
             <th>מזהה</th>
             <th>שם הלקוח</th>
             <th>אימייל</th>
+            <th>סטטוס הזמנה</th>
             <th>מפתח רישיון</th>
-            <th>סטטוס</th>
+            <th>סטטוס רישיון</th>
             <th>תאריך תפוגה</th>
             <th>תאריך רכישה</th>
             <th>סכום רכישה</th>
@@ -52,6 +56,7 @@ function render_license_management_page() {
                 <td>{$license->ID}</td>
                 <td>{$license->display_name}</td>
                 <td>{$license->user_email}</td>
+                <td>{$license->post_status}</td>
                 <td>{$license->license_key}</td>
                 <td>{$license->license_status}</td>
                 <td>{$license->expiry_date}</td>
@@ -68,6 +73,7 @@ function render_license_management_page() {
 
     echo "</tbody></table></div>";
 }
+
 
 // שינוי סטטוס רישיון
 function update_license_status() {
